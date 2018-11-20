fcd8741e15b5eb36b278e79b7622ca826c419f07
Jiahao Huo <alexhuo@dyn-129-236-225-18.dyn.columbia.edu>
2018-11-20 13:26:40 -0500

    Add Event and Reminder Class

---
+++ Event_class.py
@@ -0,0 +1,8 @@
+class Event:
+
+    def __init__(self,item,remind_time = None,priority = None, notes = None):
+        self.item = item
+        self.remind_time = remind_time
+        self.priority = priority
+        self.notes = notes
+
--- Reminder_class.py
+++ Reminder_class.py
@@ -1,7 +1,13 @@
-class Remainder:
+class Reminder:
+
+    #class attribute 
+    event_lst = list()
+
     def __init__(self,event,remind_date = None,remind_time = None,priority=None,notes=None):
         self.event = event
         self.remind_date = remind_date
         self.remind_time = remind_time
         self.priority = priority
         self.notes = notes
+
+    def 
