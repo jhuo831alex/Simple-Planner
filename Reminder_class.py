00dc7662e63bc344b6b896f3294c238f82f06546
Jiahao Huo <alexhuo@dyn-129-236-225-18.dyn.columbia.edu>
2018-11-20 13:30:35 -0500

    Add reminder class instantiation

--- Reminder_class.py
+++ Reminder_class.py
@@ -1,13 +1,4 @@
 class Reminder:
 
-    #class attribute 
-    event_lst = list()
-
-    def __init__(self,event,remind_date = None,remind_time = None,priority=None,notes=None):
-        self.event = event
-        self.remind_date = remind_date
-        self.remind_time = remind_time
-        self.priority = priority
-        self.notes = notes
-
-    def 
+    def __init__(self,category = reminder_):
+        self.category = category
