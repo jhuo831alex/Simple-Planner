e2398c2404c2b9d02f4323150a5469c99edc5515
Jiahao Huo <alexhuo@dyn-129-236-224-253.dyn.columbia.edu>
2018-11-25 17:25:29 -0500

    Add checkbox functionality

--- GUI.py
+++ GUI.py
@@ -30,7 +30,6 @@ class ReminderGUI:
 
     def __init__(self,master):
         self.event_list = list()
-        self.var_lst = list()
         self.button_lst = list()
 
         master.title('Simple Reminder')
@@ -44,15 +43,6 @@ class ReminderGUI:
         ttk.Button(self.frame,text='Add',command = self.update_reminder).grid(row = 0, column = 2,pady = 5)
 
 
-        #ttk.Button(self.frame, text = 'Add Task',command = self.add_task).grid(row = len(self.event_list)+1, column = 0, padx = 5, pady = 5)
-
-
-    def naccheck(self, check,var):
-        if var.get() == 0:
-            check.configure(state=DISABLED)
-        else:
-            check.configure(state=NORMAL)
-
     def removeCheckButton(self,button_num):
         self.button_lst[button_num].destroy()
 
@@ -63,29 +53,15 @@ class ReminderGUI:
         self.event_list.append(new_event)
         n = len(self.event_list)
         var = IntVar()
-        check = Checkbutton(self.frame,text=str(self.event_list[n-1]),variable=var).grid(row=n+2,column=0,sticky=W,columnspan=2)
-        self.var_lst.append(var)
+
+        check = Checkbutton(self.frame,
+                    text=self.event_list[n-1],
+                    variable=self.event_list[n-1],
+                    command=lambda ni=n-1: self.removeCheckButton(ni))
+        
+        check.grid(row=n+2,column=0,sticky=W,columnspan=2)
         self.button_lst.append(check)
 
-        for i in range(len(self.var_lst)):
-            print (self.var_lst[i].get())
-            if self.var_lst[i].get() == 1:
-                self.button_lst[i].destroy()
-                self.button_lst.pop(i)
-                self.var_lst.pop(i)
-
-            
-        
-
-        # for i in range(len(self.event_list)):
-        #     var = IntVar()
-        #     Checkbutton(self.frame,text=str(self.event_list[i]),variable=var).grid(row=i+2,column=0,sticky=W,columnspan=2)
- 
-
-        
-
-
-              
 root = Tk()
 reminder_gui = ReminderGUI(root)
 root.mainloop()
