f071781e60018be0fad78680ce2693f87d2f6997
Yingyan Zhao <yz3430@columbia.edu>
2018-11-26 09:31:08 -0500

    Fix integration script

--- Main.py
+++ Main.py
@@ -3,12 +3,27 @@ from Event_class import Event
 
 new_reminder = Reminder()
 message = "Welcome to our awesome reminder!"
-ask_User = input("What would you like to do? (Add Event/Modify Event/Check Event)")
-if ask_User in ["Add Event", "add event","Add event", "Add","add"]:
-    new_reminder.add_event()
-    print(new_reminder)
-elif ask_User in ["Modify Event","modify event","Modify event", "Modify","modify"]:
-    new_reminder.event_list.modify()
-elif ask_User in ["Check Event", "check event", "Check event", "Check", "check"]:
-    new_reminder.check_event()
-finish = "Finished!"
+While True:
+    ask_User = input("What would you like to do? (Add Event/Modify Event/Check Event/Quit)")
+    if ask_User in ["Add Event", "add event","Add event", "Add","add"]:
+        new_reminder.add_event()
+        print(new_reminder)
+    elif ask_User in ["Modify Event","modify event","Modify event", "Modify","modify"]:
+        print(new_reminder)
+        while True:
+            question = input("Which event would you like to modify? (Event number)") 
+            if question in new_reminder.event_list:
+                question.modify()
+                print(new_reminder)
+            else:
+                print("No such event.")
+                break
+    elif ask_User in ["Check Event", "check event", "Check event", "Check", "check"]:
+        new_reminder.check_event()
+        print(new_reminder)
+    elif ask_User in ["Quit", "quit"]:
+        print("Byebye.")
+        break
+    else:
+        print("Invalid input. Please refer to example.")
+exit()
