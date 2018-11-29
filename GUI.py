914d44099d8832e287f3abeaa8ed82054db96b73
Jiakai Liang <kaikaileung@dyn-209-2-209-161.dyn.columbia.edu>
2018-11-29 15:57:35 -0500

    fix bugs

--- GUI.py
+++ GUI.py
@@ -80,18 +80,27 @@ class ReminderGUI:
         min_value = self.minvar.get()
         priority = self.var.get()
 
-        self.monthvar.trace('w',month_value)
-        self.monthvar.set('mm')
-        self.dayvar.trace('w',day_value)
-        self.dayvar.set('dd')
-        self.yearvar.trace('w',year_value)
-        self.yearvar.set('yyyy')
-        self.hourvar.trace('w',hour_value)
-        self.hourvar.set('hr')
-        self.minvar.trace('w',min_value)
-        self.minvar.set('min')
-        self.var.trace('w',priority)
-        self.var.set('None')
+        if month_value != 'mm' and day_value != 'dd' and year_value != 'yyyy':
+            self.monthvar.trace('w',month_value)
+            self.monthvar = StringVar()
+            self.monthvar.set('mm')
+            self.dayvar.trace('w',day_value)
+            self.dayvar = StringVar()
+            self.dayvar.set('dd')
+            self.yearvar.trace('w',year_value)
+            self.yearvar = StringVar()
+            self.yearvar.set('yyyy')
+            if hour_value != 'hr' and min_value != 'min':
+                self.hourvar.trace('w',hour_value)
+                self.hourvar = StringVar()
+                self.hourvar.set('hr')
+                self.minvar.trace('w',min_value)
+                self.minvar = StringVar()
+                self.minvar.set('min')
+        if priority != 'None':
+            self.var.trace('w',priority)
+            self.var = StringVar()
+            self.var.set('None')
 
         if task: 
             self.task_name.delete(0,END)
@@ -99,6 +108,19 @@ class ReminderGUI:
             self.event_list.append(task)
             self.time_lst.append([month_value,day_value,year_value,hour_value,min_value])
             self.priority_lst.append(priority)
+
+            # self.monthvar.trace('w',month_value)
+            # self.monthvar.set('mm')
+            # self.dayvar.trace('w',day_value)
+            # self.dayvar.set('dd')
+            # self.yearvar.trace('w',year_value)
+            # self.yearvar.set('yyyy')
+            # self.hourvar.trace('w',hour_value)
+            # self.hourvar.set('hr')
+            # self.minvar.trace('w',min_value)
+            # self.minvar.set('min')
+            # self.var.trace('w',priority)
+            # self.var.set('None')
 
             if len(self.event_list)==1:
                 Label(self.frame,text='Reminder List',
@@ -113,7 +135,6 @@ class ReminderGUI:
                 if hour_value != 'hr' and min_value != 'min':
                     display_text += hour_value + ':' + min_value
             
-
             n = len(self.event_list)
             var = IntVar()
 
