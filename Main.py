f56cda7929aa206c1a98e3bea06100b2c4d25a57
Yingyan Zhao <yz3430@columbia.edu>
2018-11-30 00:15:43 -0500

    add function in main and develop add event test cases

--- Main.py
+++ Main.py
@@ -3,30 +3,31 @@
 from Reminder_class import Reminder
 from Event_class import Event
 
-new_reminder = Reminder()
-message = "Welcome to our awesome reminder!"
-while True:
-    ask_User = input("What would you like to do? (Add Event/Modify Event/Check Event/Quit)")
-    if ask_User in ["Add Event", "add event","Add event", "Add","add"]:
-        new_reminder.add_event()
-        print(new_reminder)
-    elif ask_User in ["Modify Event","modify event","Modify event", "Modify","modify"]:
-        print(new_reminder)
-        while True:
-            question = input("Which event would you like to modify? (Event number)") 
-            try:
-                new_reminder.event_list[int(question)-1].modify()
-                print(new_reminder)
-                break
-            except:
-                print("No such event.")
-                break            
-    elif ask_User in ["Check Event", "check event", "Check event", "Check", "check"]:
-        new_reminder.check_event()
-        print(new_reminder)
-    elif ask_User in ["Quit", "quit"]:
-        print("Byebye.")
-        break
-    else:
-        print("Invalid input. Please refer to example.")
+def main():
+    new_reminder = Reminder()
+    message = "Welcome to our awesome reminder!"
+    while True:
+        ask_User = input("What would you like to do? (Add Event/Modify Event/Check Event/Quit)")
+        if ask_User in ["Add Event", "add event","Add event", "Add","add"]:
+            new_reminder.add_event()
+            print(new_reminder)
+        elif ask_User in ["Modify Event","modify event","Modify event", "Modify","modify"]:
+            print(new_reminder)
+            while True:
+                question = input("Which event would you like to modify? (Event number)") 
+                try:
+                    new_reminder.event_list[int(question)-1].modify()
+                    print(new_reminder)
+                    break
+                except:
+                    print("No such event.")
+                    break            
+        elif ask_User in ["Check Event", "check event", "Check event", "Check", "check"]:
+            new_reminder.check_event()
+            print(new_reminder)
+        elif ask_User in ["Quit", "quit"]:
+            print("Byebye.")
+            break
+        else:
+            print("Invalid input. Please refer to example.")
 
